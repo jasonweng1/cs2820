0c3c66423645ee457a679f1eb8c1eefc4dfcf263
haiweng <hai-weng@uiowa.edu>
2023-09-06 14:50:42 -0500

    Print to the console graph name and each flow

--- src/edu/uiowa/cs/warp/WorkLoadDescription.java
+++ src/edu/uiowa/cs/warp/WorkLoadDescription.java
@@ -2,6 +2,8 @@
  * 
  */
 package edu.uiowa.cs.warp;
+import java.util.Arrays;
+import java.util.Collections;
 
 /**
  * Reads the input file, whose name is passed as input parameter to the constructor, and builds a
@@ -58,4 +60,66 @@ public class WorkLoadDescription extends VisualizationObject {
     this.inputFileName = gf.getGraphFileName();
     description = new Description(inputGraphString);
   }
+  
+  public static String unbrace(String braceful) {
+	// Get rid of the braces and subsequent empty lines
+	braceful = braceful.replaceAll("\\{", "");
+	braceful = braceful.replaceAll("\\}", "");
+	braceful = braceful.replaceAll("(?m)^[ \\t]*\\r?\\n", "\n");
+	return braceful;
+  }
+  
+  public static String[] splitString(String unsplitted) {
+	// Split the string into the title and the data
+	String[] stringSplit = unsplitted.split("\n", 2);
+	return stringSplit;
+  }
+  
+  public static String[] sort(String unsorted) {
+	// Put the data into an array, sort it, and reverse it
+	String[] linesArray = unsorted.split("\n");
+	Arrays.sort(linesArray);
+	Collections.reverse(Arrays.asList(linesArray));
+	return linesArray;
+  }
+  
+  public static String[] label(String[] data) {
+	// Label each line of data with "Flow x: "
+	for (int i = 0; i < data.length; i++) {
+		data[i] = "Flow " + (i + 1) + ": " + data[i];
+	}
+	return data;
+  }
+  
+  public static String format(String text) {
+	  // Process the text into organized data (with the methods above)
+	  text = unbrace(text);
+	  
+	  String title = splitString(text)[0];
+	  String data = splitString(text)[1];
+	  
+	  String[] sortedData = sort(data);
+	  String[] labeledData = label(sortedData);
+
+	  // Return the title combined with the (finally labeled and sorted) data
+	  String result = "";
+	  
+	  result += title;
+	  for (int i = 0; i < labeledData.length; i++) {
+		  result += labeledData[i];
+	  }
+	  
+	  return result;
+  }
+
+  public static void main(String[] args) {
+	  // Make a new WorkLoadDescription and get its contents as a string
+	  WorkLoadDescription test = new WorkLoadDescription("StressTest.txt");
+	  String testString = test.toString();
+	  
+	  // Format the string and then print the results
+	  String result = format(testString);
+	  System.out.println(result);
+  }
+  
 }
